b9252e761e413f7b26b338dd2f3d1f408b913bce
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-03-14 20:54:04 -0300

    hierarchy

--- models.py
+++ models.py
@@ -17,3 +17,13 @@ class Profile(object):
 
 	def print(self):
 		print 'Name: %s, Telephone: %s, Company: %s, Likes: %s' % (self.name, self.telephone, self.company, self.__likes)
+
+class Profile_Vip(Profile):
+	'User vip profile class'
+
+	def __init__(self, name, telephone, company, nickname):
+		super(Profile_Vip, self).__init__(name, telephone, company)
+		self.nickname = nickname
+
+	def credits(self):
+		return super(Profile_Vip, self).likes() * 10.0
