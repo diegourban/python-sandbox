9976f6c6e43559e6d13ef9d666b8e35e989faeb1
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-12-30 10:43:48 -0200

    ajuste

--- jogos/forca.py
+++ jogos/forca.py
@@ -117,7 +117,7 @@ def desenha_forca(erros):
 def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
     index = 0
     for letra in palavra_secreta:
-        if (chute == letra):index
+        if (chute == letra):
             letras_acertadas[index] = letra;
         index += 1
 
